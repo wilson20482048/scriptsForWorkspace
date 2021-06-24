f93007ea8cb1ef08fe476534e9d2fbaf71c9760f
wilson20482048 <62437129+wilson20482048@users.noreply.github.com>
2021-06-24 12:49:05 +0200

    Create gybRestore.sh

---
+++ gybRestore.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+gam=/root/bin/gamadv-xtd3/gam
+gyb=/root/bin/gyb/gyb
+arch=user.archive
+
+echo "Email address to copy messages from:"
+read copyFrom
+echo "Target email address:"
+read copyTo
+echo "Label of restored emails:"
+read label
+
+$gam user @arch get drivefile name $copyFrom".tar.gz"
+tar -zxvf /root/Downloads/$copyFrom".tar.gz" -C /
+$gyb --email $copyTo --local-folder /root/backup/$copyFrom --action restore --service-account --label-restored $labelrm -r /root/backup/$copyFrom /root/Downloads/$copyFrom".tar.gz"
