f92c6aaf4b87c182e5defe9765e8d73ef821b114
wilson20482048 <62437129+wilson20482048@users.noreply.github.com>
2021-07-23 16:24:48 +0200

    Update approveMobile.sh

--- approveMobile.sh
+++ approveMobile.sh
@@ -2,7 +2,7 @@
 gam="/root/bin/gamadv-xtd3/gam"
 usr=$@
 if [ -z $usr ]; then
-echo "./archive <email>"
+echo "./approveMobile.sh <email>"
 exit 0
 fi
 id=$($gam print mobiles query email:$usr | csvcut -c resourceId,firstSync | tail -n +2 | sort -rk2 | head -n 1 | cut -d "," -f 1)
